50c460ca46ccefd0bf1cfd636bac88f387f65923
sabilafiridas <sabilafirdias@gmail.com>
2024-06-13 11:58:34 +0700

    void preorder

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -72,3 +72,17 @@ public:
         }
     }
 
+    void preorder(Node* ptr) {
+        if (ROOT == nullptr) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != nullptr) {
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
+
+    
+
