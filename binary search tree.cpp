0428cb12ed7d30f771828660c6effdadc6a70ea6
sabilafiridas <sabilafirdias@gmail.com>
2024-06-13 11:57:06 +0700

    conditional if-else

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -53,5 +53,12 @@ public:
         parent = nullptr;
         while ((currentNode != nullptr) && (currentNode->info != element)) {
             parent = currentNode;
-            
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+    
 
