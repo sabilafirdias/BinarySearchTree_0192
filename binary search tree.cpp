86d08ff1f1efcf8bb7f4cecb7b1d3ae9cc653513
sabilafiridas <sabilafirdias@gmail.com>
2024-06-13 11:54:39 +0700

    conditional statement if-else if

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -39,5 +39,13 @@ public:
             return; //exit
         }
 
-        
+        if (element < parent->info) {       //if the value in the data field of the new node is less than that of the
+            parent->leftchild = newNode;    //make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) {   //if the value in the data field of the new node is greater than that
+            parent->rightchild = newNode;   //make the right child of the parent point to the new node
+        }
+    }
+
+    
 
